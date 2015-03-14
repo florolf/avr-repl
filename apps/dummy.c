884bb25d6a4f31a2614afabc1a610608df096b8d
Florian Larysch <fl@n621.de>
2015-03-14 20:23:25 +0100

    include more useful headers in dummy

--- apps/dummy.c
+++ apps/dummy.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdint.h>
-
 #include <util/delay.h>
 
 #include "repl.h"
+#include "io.h"
+#include "readline.h"
+#include "uart.h"
+
+#include "app.h"
 
 void init_app(void) {
 }
